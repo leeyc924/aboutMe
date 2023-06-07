b323e5ff3b7042011dfcc03a2962bbc4773f3605
leeyc924 <leeyc924@naver.com>
2023-06-07 23:48:17 +0900

    Fix className did not match error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,14 +6,10 @@ import Gnb from '@components/Gnb';
 import DefaultLayout from '../layouts/DefaultLayout';
 import StyledComponentsRegistry from './registry';
 
-export const metadata: Metadata = {
-  title: 'leeyc',
-};
-
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko">
-      <body data-theme="light">
+      <body>
         <StyledComponentsRegistry>
           <DefaultLayout>
             <Gnb />
--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import styled, { DefaultTheme, ThemeProvider } from 'styled-components';
 
 export interface DefaultLayoutProps {
@@ -10,23 +10,27 @@ export interface DefaultLayoutProps {
 type ThemeMode = 'dark' | 'light';
 export const theme: Record<ThemeMode, DefaultTheme> = {
   dark: {
-    mainBg: '#000000',
+    mainBg: '#1b1b1d',
     mainFontColor: '#eaeaea',
   },
   light: {
-    mainBg: '#fff',
-    mainFontColor: '#000',
+    mainBg: '#ffffff',
+    mainFontColor: '#000000',
   },
 };
 
 const DefaultLayout = ({ children }: DefaultLayoutProps) => {
-  // 브라우저 테마 정보 확인
-  const isBrowserDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-  const browserTheme: ThemeMode = isBrowserDarkMode ? 'dark' : 'light';
+  const [themeType, setThemeType] = useState<ThemeMode>('light');
+  useEffect(() => {
+    // 브라우저 테마 정보 확인
+    const isBrowserDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const browserTheme: ThemeMode = isBrowserDarkMode ? 'dark' : 'light';
+    setThemeType(browserTheme);
+  }, []);
 
   return (
     <>
-      <ThemeProvider theme={theme[browserTheme]}>
+      <ThemeProvider theme={theme[themeType]}>
         <Wrap>
           <Component>{children}</Component>
         </Wrap>
